52d262c510ebadd456fbaccc02fa904008b74358
lucas <s.cardosolucas@gmail.com>
2014-03-06 19:43:07 -0300

    Adicionado jogo de Cartas

--- Hello World/src/Hello_world.java
+++ Hello World/src/Hello_world.java
@@ -6,13 +6,13 @@ public class Hello_world {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String Nome;
+		String Nome = null;
 		Scanner ler = new Scanner(System.in);
 		
 		
-		System.out.println("Informe Seu Nome:");
-		Nome = ler.next();
-		System.out.println("Hello "+ Nome);
+		Nome = JOptionPane.showInputDialog("Informe Seu Nome:");
+		//Nome = ler.next();
+		JOptionPane.showMessageDialog(null, "Hello "+ Nome);
 	}
 
 }
